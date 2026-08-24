3a35a842ed80499b43e9868f9a18332d25806829
Alex77071 <navironaldo9870@gmail.com>
2026-08-24 10:07:01 -0600

    pantalla inicio

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,27 @@
 <header class="site-header">
     <div class="site-header__container">
-        <div class="site-header__logos" aria-hidden="true">
-            <div class="site-header__logo-slot"></div>
+
+        <div class="site-header__branding">
+            <img
+                src="{{ asset('images/logos/utm_logo.jpeg') }}"
+                alt="Universidad Tecnológica de la Mixteca"
+                class="site-header__logo site-header__logo--utm"
+            >
+
             <div class="site-header__divider"></div>
-            <div class="site-header__logo-slot site-header__logo-slot--wide"></div>
+
+            <img
+                src="{{ asset('images/logos/usalab_logo.jpeg') }}"
+                alt="UsaLab"
+                class="site-header__logo site-header__logo--usalab"
+            >
         </div>
 
         <h1 class="site-header__title">
             Sistema de Gestión y Análisis de Evidencias
         </h1>
 
-        <div class="site-header__spacer" aria-hidden="true"></div>
+        <div class="site-header__spacer"></div>
+
     </div>
 </header>
