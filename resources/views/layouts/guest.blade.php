e8bae10d8ecc7fed7a83e20d7112bf1db4a990cb
dancm29 <allandaniel.cruzmatias@aulavirtual.umar.mx>
2026-08-26 12:23:50 -0600

    Update guest.blade.php
    favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,20 +1,50 @@
 <!DOCTYPE html>
+
 <html lang="es">
+
 <head>
+
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SGAE - Iniciar sesión</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+
+    <title>
+        SGAE - Iniciar sesión
+    </title>
+
+    {{-- FAVICON USALAB --}}
+    <link
+        rel="icon"
+        type="image/png"
+        href="{{ asset('images/logos/usalab_logo2.png') }}"
+    >
+
+    <link
+        rel="stylesheet"
+        href="{{ asset('css/app.css') }}"
+    >
+
 </head>
+
 <body class="guest-body">
+
     <div class="guest-page">
+
         @include('components.header')
 
         <main class="guest-main">
+
             @yield('content')
+
         </main>
 
         @include('components.footer')
+
     </div>
+
 </body>
+
 </html>
