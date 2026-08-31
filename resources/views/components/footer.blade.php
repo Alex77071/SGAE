2c941f2b7eec76387d9e42661934a2a7fa25cfbd
Alex77071 <navironaldo9870@gmail.com>
2026-08-31 09:35:15 -0600

    ayuda

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,35 @@
 <footer class="site-footer">
+
     <div class="site-footer__container">
+
         <p class="site-footer__copy">
-            <span class="site-footer__copy-icon">©</span>
-            <span>2026 Universidad Tecnológica de la Mixteca</span>
+
+            <span class="site-footer__copy-icon">
+                ©
+            </span>
+
+            <span>
+                2026 Universidad Tecnológica de la Mixteca
+            </span>
+
         </p>
 
-        <a href="#" class="site-footer__help">
-            <span class="site-footer__help-icon">?</span>
-            <span>Ayuda</span>
+
+        <a
+            href="{{ route('preguntas.frecuentes') }}"
+            class="site-footer__help"
+        >
+
+            <span class="site-footer__help-icon">
+                ?
+            </span>
+
+            <span>
+                Ayuda
+            </span>
+
         </a>
+
     </div>
+
 </footer>
